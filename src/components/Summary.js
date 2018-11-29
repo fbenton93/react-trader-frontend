bd2cf0cb0fb16dbbd6d80a2b5581d6e39f5eb84b
github username <github email address>
2018-11-29 16:11:02 -0500

    added additional info to Summary component

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -7,11 +7,15 @@ const Summary = () => {
       <h2>Welcome</h2>
       <img alt="Forrest Benton Profile" src={forrest} />
       <p>
-        Welcome to React Trader. This is a sample application I've 
+        Welcome to React Trader. This is a sample application I've
         developed to practice the use of React and Redux. This app utilizes the
         <a target="_blank" rel="noopener noreferrer" href="https://iextrading.com/developer/docs/"> IEX API </a>
         to retrieve market data via the <a target="_blank" rel="noopener noreferrer" href="https://socket.io/">
         socket.io </a> websocket and basic AJAX requests. Executed trades are saved to a backend Rails API.
+      </p>
+      <p>
+        Currently, this application is not designed to handled more sophisticated trades
+        like short-selling. It is a simple buy/sell for stocks available on the IEX API.
       </p>
       <br />
       <p>
