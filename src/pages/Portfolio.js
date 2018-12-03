0f252c4a388e340895eea8b4d5127a7bc2065ce5
github username <github email address>
2018-12-03 13:49:47 -0500

    minor css on profile page - switching to branch

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import reqAuth from '../HOCs/reqAuth';
+
+import '../stylesheets/portfolio.css';
+import { Segment } from 'semantic-ui-react';
+
 
 class Portfolio extends Component {
   render() {
+
+    const { currentUser } = this.props
+
     return (
     <div id="page-portfolio" className="page">
-    Portfolio Page
+      <div id="profile">
+        <Segment.Group horizontal>
+          <Segment>User: {currentUser.username}</Segment>
+          <Segment>Balance: ${currentUser.balance}</Segment>
+          <Segment>Account Growth: (Balance + Gains)</Segment>
+        </Segment.Group>
+      </div>
+
+      <div id="holdings">
+      </div>
     </div>
   )
   }
 }
 
-export default reqAuth(Portfolio);
+function mapStateToProps(state) {
+  return {
+    currentUser: state.currentUser.user
+  }
+}
+
+export default reqAuth(connect(mapStateToProps)(Portfolio));
