a422fdf1d4528ab6acdc822725bf91695d6326dc
github username <github email address>
2019-01-29 12:52:06 -0500

    fixed gainLoss residual on logout

--- src/reducers/GainLoss.js
+++ src/reducers/GainLoss.js
@@ -2,6 +2,8 @@ export default function(state = { default: 0 },action) {
   switch(action.type) {
     case 'UPDATE_GAIN_LOSS':
       return {...state, ...action.payload};
+    case 'LOG_OUT':
+      return { default: 0 };
     default:
       return state;
   }
